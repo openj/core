18f3a8b0928218c30e258a95ffcf0bb303e15b2c
Guillaume Martres <smarter@ubuntu.com>
2011-03-21 17:35:14 +0100

    CMakeLists.txt: installation support, only tested on Linux

--- README.cmake
+++ README.cmake
@@ -1,16 +1,18 @@
-* To build:
+* To build and install:
 mkdir build && cd build
-cmake ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
+sudo make install
 
 * Run with:
-./jconsole
+jconsole
 
 * If you had unmet optional dependencies and want to
   update the Makefile after getting them, run:
 rm CMakeCache.txt
-cmake ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 
 * Cross-compile with:
 mkdir build.x && cd build.x
 cmake .. -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_C_COMPILER=arm-none-linux-gnueabi-gcc
+make
